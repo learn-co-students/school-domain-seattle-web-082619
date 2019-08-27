2911cf2681e9c90c800cd149231f2a3a28d14814
renburnett <renardburnett@gmail.com>
2019-08-27 15:58:44 -0700

    implemented school obj

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,39 @@
-# code here!
+require 'pry'
+
+class School
+    attr_accessor = :name
+    
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def roster()
+        @roster
+    end
+
+    def roster=(roster)
+        @roster = roster
+    end
+
+    def grade(num)
+        @roster[num]
+    end
+
+    def sort()
+        @roster.each do |key, val|
+            val.sort!
+        end
+        
+        @roster
+    end
+
+    def add_student(name, age)
+        if !@roster[age]
+            @roster[age] = []
+            @roster[age].push(name)
+        else
+            @roster[age].push(name)
+        end
+    end
+end
