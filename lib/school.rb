9076e945ac7e8c6c8c3e0fef94ef129ca991eb2e
github username <github email address>
2019-08-28 14:21:51 -0700

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
 # code here!
+class School
+    attr_reader :name
+    attr_accessor :roster 
+
+    def initialize(name)
+    @name = name 
+    @roster = {}
+    end 
+
+    #initialized with name and roster in the start
+
+    def add_student (name, grade)
+    
+    @roster[@grade]=[]
+    @roster[@grade]<<@name
+
+    end 
+    
+    #grade then [name] 
+
+
+
+
+end 
