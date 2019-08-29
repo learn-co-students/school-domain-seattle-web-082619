a789931146f228a739528488afbc087e91f8b7bf
kel29 <krista.loven.87@gmail.com>
2019-08-29 11:59:11 -0700

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
-# code here!
+require "pry"
+
+class School
+    attr_accessor :school_name, :roster
+
+    def initialize(school_name)
+        @school_name = school_name
+        @roster = {}
+    end
+
+    def add_student(student, grade)
+        @roster[grade] ? @roster[grade].push(student) : @roster[grade] = [student]
+    end
+
+    def grade(grade)
+        roster[grade]
+    end
+
+    def sort 
+        roster.map { |g, s| s.sort! }
+        roster
+    end
+
+    # binding.pry
+end
